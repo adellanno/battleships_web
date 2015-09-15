c632c1a96645ac68dc6203af65063cadbd56f739
Tim Harrison <mail@trwh.co.uk>
2015-09-15 14:10:26 +0100

    Tests for the end of Version 1

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
+
+  get '/new_game' do
+    erb :name
+  end
+
+  # get '/start_game' do
+  #   @name = params[:name]
+  #   erb :start_game
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -1,10 +1,24 @@
 require_relative '../spec_helper'
 
 feature 'Starting a new game' do
+
   scenario 'I am asked to enter my name' do
     visit '/'
     click_link 'New game'
     expect(page).to have_content "Whats your name?"
   end
-  
+
+  scenario 'I\'m greeted by my name' do
+    visit '/new_game'
+    fill_in 'name', :with => 'P. Face'
+    click_button 'Submit'
+    expect(page).to have_content "Hello, P. Face. Welcome to a game of Battleships."
+  end
+
+  scenario 'I\'m warned if I don\'t enter a name' do
+    visit '/new_game'
+    click_button 'Submit'
+    expect(page).to have_content "You didn't enter a name. If you don't enter a name you will be called Pooface."
+  end
+
 end
